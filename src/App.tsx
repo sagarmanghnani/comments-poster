bb245aa87761eec5b8af3fe0e493ac80ab9938d5
sagarmanghnani <sagarmanghnani@gmail.com>
2024-03-17 01:31:31 +0530

    added delete btn changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PostCreator from './components/PostCreator';
-import PostType from './enums';
-import IPostData from './post.interface';
-import { putPosts } from './post-viewer.utils';
-import CommentPost from './components/CommentPost';
-import PostViewer from './components/PostViewer';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PostCreator from "./components/PostCreator";
+import PostType from "./enums";
+import "bootstrap/dist/css/bootstrap.min.css";
+import IPostData from "./post.interface";
+import { putPosts } from "./post-viewer.utils";
+import CommentPost from "./components/CommentPost";
+import PostViewer from "./components/PostViewer";
 
 function App() {
-
-
-  const makePost = (postData:IPostData) => {
+  const makePost = (postData: IPostData) => {
     try {
-      putPosts(postData)
-    } catch(err) {
+      putPosts(postData);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="App">
-      <CommentPost ></CommentPost>
-      <PostViewer></PostViewer>
+      <CommentPost></CommentPost>
+      <PostViewer postId="1710618202720"></PostViewer>
     </div>
   );
 }
--- src/components/PostViewer.tsx
+++ src/components/PostViewer.tsx
@@ -1,37 +1,42 @@
 import Button from "react-bootstrap/esm/Button";
-import Card from "react-bootstrap/esm/Card"
+import Card from "react-bootstrap/esm/Card";
 import getPostById from "../post-viewer.utils";
 import IPostData from "../post.interface";
-import './post-viewer.scss';
+import "./post-viewer.scss";
+import { Trash3Fill } from "react-bootstrap-icons";
 
 interface IPostViewerProps {
-    postId:string;
-    onPostDeleteBtnClick?: (postId:string) => void;
+  postId: string;
+  onPostDeleteBtnClick?: (postId: string) => void;
 }
 
-const PostViewer = ({postId, onPostDeleteBtnClick}: IPostViewerProps) => {
+const PostViewer = ({ postId, onPostDeleteBtnClick }: IPostViewerProps) => {
+  const postData: IPostData | null = getPostById(postId);
 
-    const postData: IPostData | null = getPostById(postId)
+  const handlePostDelete = () => {};
 
-    return (
+  return (
+    <>
+      {postData && (
         <>
-            {postData && <> 
-            <Card className="post-viewer-container">
-                <Card.Body>
-                    <h5> {postData?.name} </h5>
-                    <span className="comment"> {postData?.comment} </span>
-                </Card.Body>
+          <Card className="post-viewer-container">
+            <Card.Body>
+              <h5> {postData?.name} </h5>
+              <span className="comment"> {postData?.comment} </span>
+            </Card.Body>
 
-                <div className="footer-actions">
-                    <Button variant="link"> Reply </Button>
-                    <Button variant="link"> Edit </Button>
-                </div>
-            </Card>
-            <button> Delete </button>
-            </>
-            }
+            <div className="footer-actions">
+              <Button variant="link"> Reply </Button>
+              <Button variant="link"> Edit </Button>
+            </div>
+            <button className="delete-btn">
+              <Trash3Fill />
+            </button>
+          </Card>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
 export default PostViewer;
